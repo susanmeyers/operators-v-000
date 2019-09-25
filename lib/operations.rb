d6d04906235f4779b27993302a9f748b91252c46
Susan Meyers <smmeyers24@gmail.com>
2019-09-25 02:22:59 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,28 @@
+require 'pry'
 def unsafe?(speed)
-
+  if speed > 60
+   return true
+  end
+    #binding.pry
+  if speed < 40
+    return true
+  end
+      
 end
+   
+
+     
+   
+   
+     
+
+
 
 
 
 def not_safe?(speed)
 	
 end
-	
+
 
 
